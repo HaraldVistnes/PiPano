909d82ccb30a98babe436347525c3659dcf6641a
Harald Vistnes <harald.vistnes@gmail.com>
2013-11-17 14:55:37 +0100

    Updated PiPano.py

--- PiPano.py
+++ PiPano.py
@@ -37,35 +37,35 @@ timestamp = time.strftime("%Y%m%d_%H%M", now)
 opt = " -w 2592 -h 1944 -hf -vf -n -q 100 -t 2000 -sh 0 -ISO 100 -awb horizon"
 
 def forward(delay, steps):
-        for i in range(0, steps):
-                setStep(1, 0, 1, 0)
-                time.sleep(delay)
-                setStep(0, 1, 1, 0)
-                time.sleep(delay)
-                setStep(0, 1, 0, 1)
-                time.sleep(delay)
-                setStep(1, 0, 0, 1)
-                time.sleep(delay)
+	i in range(0, steps):
+		setStep(1, 0, 1, 0)
+		time.sleep(delay)
+		setStep(0, 1, 1, 0)
+		time.sleep(delay)
+		setStep(0, 1, 0, 1)
+		time.sleep(delay)
+		setStep(1, 0, 0, 1)
+		time.sleep(delay)
 
 def backward(delay, steps):
-        for i in range(0, steps):
-                setStep(1, 0, 0, 1)
-                time.sleep(delay)
-                setStep(0, 1, 0, 1)
-                time.sleep(delay)
-                setStep(0, 1, 1, 0)
-                time.sleep(delay)
-                setStep(1, 0, 1, 0)
-                time.sleep(delay)
+	i in range(0, steps):
+		setStep(1, 0, 0, 1)
+		time.sleep(delay)
+		setStep(0, 1, 0, 1)
+		time.sleep(delay)
+		setStep(0, 1, 1, 0)
+		time.sleep(delay)
+		setStep(1, 0, 1, 0)
+		time.sleep(delay)
 
 def setStep(w1, w2, w3, w4):     
-        GPIO.output(coilA1Pin, w1) 
-        GPIO.output(coilA2Pin, w2)
-        GPIO.output(coilB1Pin, w3) 
-        GPIO.output(coilB2Pin, w4)
+	GPIO.output(coilA1Pin, w1) 
+	GPIO.output(coilA2Pin, w2)
+	GPIO.output(coilB1Pin, w3) 
+	GPIO.output(coilB2Pin, w4)
 
 def release():
-        setStep(0, 0, 0, 0)
+	setStep(0, 0, 0, 0)
 
 def capture(arg):
 	os.system("raspistill -o " + dir + "/img_" + timestamp + "_" + arg + ".jpg" + opt)
@@ -78,7 +78,7 @@ backward(delay, halfSteps)
 
 for x in range(0, numSteps):
 	capture(x+1)
-	forward(delay, 1)
+	forward(delay, 1)	
 	time.sleep(1.0)
 
 backward(delay, halfSteps)
