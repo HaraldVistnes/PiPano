9c04e659bf220b67a996bf3f1ef5f76d978bc6ef
Harald Vistnes <harald.vistnes@gmail.com>
2013-11-17 19:22:30 +0100

    Updated PiPano.py

--- PiPano.py
+++ PiPano.py
@@ -17,7 +17,7 @@ delay = 0.01
 
 dir = "/mnt/usb/images"
 if (not os.path.isdir(dir)):
-    dir = "/home/pi/images"
+	dir = "/home/pi/images"
 
 
 GPIO.setmode(GPIO.BCM)
@@ -36,8 +36,14 @@ now = time.localtime(time.time())
 timestamp = time.strftime("%Y%m%d_%H%M", now)
 opt = " -w 2592 -h 1944 -hf -vf -n -q 100 -t 2000 -sh 0 -ISO 100 -awb horizon"
 
+def setStep(w1, w2, w3, w4):     
+	GPIO.output(coilA1Pin, w1) 
+	GPIO.output(coilA2Pin, w2)
+	GPIO.output(coilB1Pin, w3) 
+	GPIO.output(coilB2Pin, w4)
+
 def forward(delay, steps):
-	i in range(0, steps):
+	for i in range(steps):
 		setStep(1, 0, 1, 0)
 		time.sleep(delay)
 		setStep(0, 1, 1, 0)
@@ -48,7 +54,7 @@ def forward(delay, steps):
 		time.sleep(delay)
 
 def backward(delay, steps):
-	i in range(0, steps):
+	for i in range(steps):
 		setStep(1, 0, 0, 1)
 		time.sleep(delay)
 		setStep(0, 1, 0, 1)
@@ -57,12 +63,6 @@ def backward(delay, steps):
 		time.sleep(delay)
 		setStep(1, 0, 1, 0)
 		time.sleep(delay)
-
-def setStep(w1, w2, w3, w4):     
-	GPIO.output(coilA1Pin, w1) 
-	GPIO.output(coilA2Pin, w2)
-	GPIO.output(coilB1Pin, w3) 
-	GPIO.output(coilB2Pin, w4)
 
 def release():
 	setStep(0, 0, 0, 0)
