41fd7206fb1858699c2d7d90e3ebd0072e13cc32
Harald Vistnes <harald.vistnes@gmail.com>
2013-11-29 14:02:17 +0100

    Updated temp.py

--- temp.py
+++ temp.py
@@ -25,11 +25,25 @@ class DS18B20:
         f.close()
         return lines
 
+    def read_temp():
+        lines = read_temp_raw()
+        while lines[0].strip()[-3:] != 'YES':
+            time.sleep(0.2)
+            lines = read_temp_raw()
+        equals_pos = lines[1].find('t=')
+        if equals_pos != -1:
+            temp_string = lines[1][equals_pos+2:]
+            temp_c = float(temp_string) / 1000.0
+            return temp_c
+        else:
+            return -1000.0 # something is wrong!
+
+
 if __name__ == "__main__":
     # script
     GPIO.setmode(GPIO.BCM)
     ds18b20 = DS18B20()
     while True:
-        print ds18b20.read_temp_raw()
+        print ds18b20.read_temp()
         time.sleep(1.0)
 
