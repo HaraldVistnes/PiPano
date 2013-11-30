383ec3809f1de6c3f2c6208275103a35e32183f1
Harald Vistnes <harald.vistnes@gmail.com>
2013-11-30 20:18:22 +0100

    Read 2 sensors

--- temp.py
+++ temp.py
@@ -6,6 +6,9 @@
     This implementation is based on the tutorial
     http://learn.adafruit.com/adafruits-raspberry-pi-lesson-11-ds18b20-temperature-sensing/
 
+    NB! The DS18B20 temperature sensor MUST be connected to 
+    GPIO #4 as that is the only pin that is supported by
+    the 1-Wire protocol.
 """
 
 import RPi.GPIO as GPIO, time, os, glob
@@ -15,17 +18,18 @@ class DS18B20:
         os.system('modprobe w1-gpio')
         os.system('modprobe w1-therm')  
 
+        self.sensor_id_std = '28-000004619c93'   # standard sensor
+        self.sensor_id_wp  = '28-0000049861cc'   # waterproof sensor
         self.base_dir = '/sys/bus/w1/devices/'
-        self.device_folder = glob.glob(self.base_dir + '28*')[0]
-        self.device_file = self.device_folder + '/w1_slave'
 
-    def read_temp_raw(self):
-        f = open(self.device_file, 'r')
+    def read_temp_raw(self, sensor_id):
+        filename = self.self.base_dir + sensor_id + '/w1_slave'
+        f = open(filename, 'r')
         lines = f.readlines()
         f.close()
         return lines
 
-    def read_temp(self):
+    def read_temp(self, sensor_id):
         lines = self.read_temp_raw()
         while lines[0].strip()[-3:] != 'YES':
             time.sleep(0.2)
@@ -38,12 +42,14 @@ class DS18B20:
         else:
             return -1000.0 # something is wrong!
 
+    def read_temps(self):
+        return (self.read_temp(self.sensor_id_std), self.read_temp(self.sensor_id_wp))            
 
 if __name__ == "__main__":
     # script
     GPIO.setmode(GPIO.BCM)
     ds18b20 = DS18B20()
     while True:
-        print ds18b20.read_temp()
+        print ds18b20.read_temps()
         time.sleep(1.0)
 
