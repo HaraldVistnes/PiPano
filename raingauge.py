16cca4a294ab6db6ae8d822a5b9198d79ff0586b
Harald Vistnes <harald.vistnes@gmail.com>
2013-12-29 17:56:28 +0100

    Rain gauge

---
+++ raingauge.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+""" Module for reading the temperature using a DS18B20 1-wire temperature sensor.
+
+    Copyright (C) 2013 Harald Vistnes <harald.vistnes@gmail.com>
+
+    This implementation is based on the tutorial
+    http://learn.adafruit.com/adafruits-raspberry-pi-lesson-11-ds18b20-temperature-sensing/
+
+    NB! The DS18B20 temperature sensor MUST be connected to 
+    GPIO #4 as that is the only pin that is supported by
+    the 1-Wire protocol.
+"""
+
+import RPi.GPIO as GPIO, time, os, glob
+
+
+if __name__ == "__main__":
+    # script
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+    while (True):
+        GPIO.wait_for_edge(22, GPIO.RISING)
+        print('PLING!')
+
